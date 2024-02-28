6b6c3c5433f83e5921866e6f642e5beab34231bc
weird-bytecode <140429024+weird-bytecode@users.noreply.github.com>
2024-02-28 15:18:13 -0600

    chore: add user id to analytics where applicable

--- common/analytics.js
+++ common/analytics.js
@@ -5,23 +5,45 @@ import googleAnalytics from "@analytics/google-analytics";
  * Initilize the analytics library and attach it to window._mdai_analytics. See https://www.npmjs.com/package/analytics for more info on usage
  * @param {String} appName The name of the app, possibly mdai-console, or mdai-core-ui-system-demosite
  * @param {String} googleAnalyticsId Google analytics measurement ID (use env var!)
+ * @param {String=} userId The user ID if applicable
  * @param {String=} engineId The engine ID if applicable
  */
-export const initializeAnalytics = (appName, googleAnalyticsId, engineId) => {
-  if (!appName || !googleAnalyticsId) {
-    return null;
+export const initializeAnalytics = (
+  appName,
+  googleAnalyticsId,
+  userId,
+  engineId
+) => {
+  try {
+    if (!appName || !googleAnalyticsId) {
+      return null;
+    }
+
+    const analytics = analyticsCtor({
+      app: appName,
+      plugins: [
+        googleAnalytics({
+          measurementIds: [googleAnalyticsId],
+          debug: import.meta.env.DEV,
+        }),
+      ],
+    });
+
+    if (userId) {
+      analytics.identify(
+        userId,
+        engineId
+          ? {
+              engineId,
+            }
+          : {}
+      );
+    }
+
+    window._mdai_analytics = analytics;
+    return analytics;
+  } catch (error) {
+    console.log(error);
+    // TODO: Do something with this.
   }
-
-  const analytics = analyticsCtor({
-    app: appName,
-    plugins: [
-      googleAnalytics({
-        measurementIds: [googleAnalyticsId],
-        debug: import.meta.env.DEV,
-      }),
-    ],
-  });
-
-  window._mdai_analytics = analytics;
-  return analytics;
 };
