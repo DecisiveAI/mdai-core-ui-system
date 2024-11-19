ca0ec36afd7a0041abe77e0ea5dda3a9dfd27e9e
Joe Churchman <joe@mydecisive.ai>
2024-11-19 15:03:35 -0800

    chore: Remove addon-svelte-csf pkg. conflicted with svelte 5

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from "@storybook/svelte-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
   addons: [
-    "@storybook/addon-svelte-csf",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
