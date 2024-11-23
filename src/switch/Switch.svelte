1edf64e28470704874cedafffff66badf244ecd4
Joe Churchman <joe@mydecisive.ai>
2024-11-22 16:02:22 -0800

    chore: Cherry pick from previous branch

---
+++ src/switch/Switch.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Switch from '@smui/switch';
+    import './switch.css';
+
+    type SwitchProps = {
+        onClick: () => void;
+        class?: string;
+        disabled?: boolean;
+        checked: boolean;
+        icons?: boolean;
+    }
+
+    let {
+        onClick,
+        class: className,
+        disabled,
+        checked,
+        icons = true,
+    }: SwitchProps = $props();
+
+</script>
+
+<Switch {checked} {disabled} {icons} class={className} {onClick}/>
+
