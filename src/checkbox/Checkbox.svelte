353639c1a7ce3b8c35670c6c09f086f814c7c22d
Joe Churchman <joe@mydecisive.ai>
2024-12-09 10:29:11 -0800

    chore: Checkbox WIP

---
+++ src/checkbox/Checkbox.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import SMUICheckbox from '@smui/checkbox';
+    
+    import './checkbox.css';
+
+    type CheckboxProps = {
+        checked?: boolean;
+        disabled?: boolean;
+        indeterminate?: boolean;
+    }
+
+    let {
+        checked,
+        disabled,
+        indeterminate,
+    }: CheckboxProps = $props();
+
+</script>
+
+<SMUICheckbox
+    {checked}
+    {disabled}
+    {indeterminate}
+/>
+
