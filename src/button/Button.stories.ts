fbd7005c845768076082167c480ba7fa90df78fb
Shalane Proctor <shalane@mydecisive.ai>
2024-12-12 17:09:19 -0600

    Chore removed unused testing from button

--- src/button/Button.stories.ts
+++ src/button/Button.stories.ts
@@ -1,6 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
-import { fn } from '@storybook/test';
-import { userEvent, within } from '@storybook/test';
 import { withActions } from '@storybook/addon-actions/decorator';
 import Button from './Button.svelte';
 
@@ -13,13 +11,6 @@ const meta = {
     variant: {
       control: { type: 'select' },
       options: ['Text', 'Outlined', 'Filled'],
-    },
-    onClick: fn(),
-  },
-  parameters: {
-    actions: {
-      handles: ['click'],
-      argTypesRegex: '^on.*',
     },
   },
   decorators: [withActions],
@@ -34,13 +25,8 @@ export const Raised: Story = {
     variant: 'Filled',
     label: 'Button',
     icon: 'add',
-    onClick: () => console.log('clicked'),
+    handleChange: () => console.log('clicked'),
   },
-  play: async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const button = canvas.getByRole('button');
-  await userEvent.click(button);
-}
 };
 
 export const Outlined: Story = {
